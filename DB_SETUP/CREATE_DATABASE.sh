cbb78bee511492b18871c29a7ff0b0ac5b409879
fida <fiidaawan@gmail.com>
2022-10-25 14:35:08 +0500

    initial commit

--- DB_SETUP/CREATE_DATABASE.sh
+++ DB_SETUP/CREATE_DATABASE.sh
@@ -1,31 +1,17 @@
+
+# ORIGNAL CODE
 #!/bin/bash
-echo "Creating Resource-Group"
+
+echo "Beginning database creation process..."
 az group create -l eastus -n tailwind_rg
 
+groupName=tailwind_rg
 rnd=$RANDOM
 accountName=tailwind-traders-$RANDOM
 databaseName=tailwind
 containerName=products
 
 echo "Beginning database creation process..."
-groupName=tailwind_rg
-
 echo "Creating Cosmos DB database $accountName in Resource Group $groupName..."
 echo "This can take up to 10 minutes. Feel free to continue with the Learn Module."
 az cosmosdb create -n $accountName -g $groupName -o none
-
-
-# # ORIGNAL CODE
-# #!/bin/bash
-# rnd=$RANDOM
-# accountName=tailwind-traders-$RANDOM
-# databaseName=tailwind
-# containerName=products
-
-# echo "Beginning database creation process..."
-
-# groupName=$(az group list --query "[4].name" -o tsv)
-
-# echo "Creating Cosmos DB database $accountName in Resource Group $groupName..."
-# echo "This can take up to 10 minutes. Feel free to continue with the Learn Module."
-# az cosmosdb create -n $accountName -g $groupName -o none
