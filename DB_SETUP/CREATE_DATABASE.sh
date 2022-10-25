6788475ec4248a8790d2c4ee414ed3c1f336ef89
Fida <73682723+Fidasultan@users.noreply.github.com>
2022-10-25 15:54:43 +0500

    Update CREATE_DATABASE.sh

--- DB_SETUP/CREATE_DATABASE.sh
+++ DB_SETUP/CREATE_DATABASE.sh
@@ -1,17 +1,14 @@
 
-# ORIGNAL CODE
 #!/bin/bash
-
-echo "Beginning database creation process..."
-az group create -l eastus -n tailwind_rg
-
-groupName=tailwind_rg
 rnd=$RANDOM
 accountName=tailwind-traders-$RANDOM
 databaseName=tailwind
 containerName=products
 
 echo "Beginning database creation process..."
+
+groupName=$(az group list --query "[0].name" -o tsv)
+
 echo "Creating Cosmos DB database $accountName in Resource Group $groupName..."
 echo "This can take up to 10 minutes. Feel free to continue with the Learn Module."
 az cosmosdb create -n $accountName -g $groupName -o none
